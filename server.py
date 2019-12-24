e9e77d27edcf6353e4a6fb64a430faaf02cb717e
nect88 <55210686+nect88@users.noreply.github.com>
2019-12-24 18:44:48 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,15 +1,101 @@
+# -*- coding: utf-8 -*-
 import socket
-import pickle
+import csv, os
 
-HOST = '127.0.0.1'
-PORT = 8080
+file = os.path.join(os.getcwd(), 'keys.csv')
+with open (os.path.join(os.getcwd(), 'servkeys.csv'), 'r') as f:
+    i = 0
+    for line in csv.reader(f):
+        if i == 0: 
+            p = int(''.join(line))
+        elif i == 1:
+            g = int(''.join(line))
+        elif i == 2:
+            a = int(''.join(line))
+        i += 1
 
-sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
-conn, addr = sock.accept()
+def ps_key_gen(conn, p, g):
+    with open (os.path.join(os.getcwd(), 'servkeys.csv'), 'r') as f:
+        i = 0
+        for line in csv.reader(f):
+            if i == 3: 
+                Kps = int(''.join(line))
+            i += 1
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+    msg = str(Kps) + ' ' + str(g) + ' ' + str(p)
+    conn.send(msg.encode())
+    return Kps
 
-conn.close()
+def total_key_gen(Kpc):
+    global p, g, a
+    K = (int(Kpc)**a) % p
+    return K
+    
+def check_ksc(Kpc):
+    global file
+    with open (file, 'r') as f:
+        know = False
+        for line in csv.reader(f):
+            if Kpc in line:
+                know = True
+                return True
+        if know == False:
+            return False
+                
+
+def cript(msg, k):
+    msgl = list(msg)
+    for i in range(len(msgl)):
+        msgl[i] = chr(ord(msgl[i])+k)
+    msg = ''.join(msgl)
+    return msg
+
+def decript(msg, k):
+    msgl = list(msg)
+    for i in range(len(msgl)):
+        msgl[i] = chr(ord(msgl[i])-k)
+    msg = ''.join(msgl)
+    return msg
+
+def main():
+
+    sock = socket.socket()
+    sock.bind(('', 9092))
+    sock.listen(0)
+    
+    while True:
+        
+        conn, addr = sock.accept()
+        print(addr)
+        
+        ps_key_gen(conn, p, g)
+        
+        Kpc = conn.recv(1024).decode()
+        print("Client's public key recieved")
+        if check_ksc(Kpc) == True:
+            print("Client found")
+            pass
+        else:
+            print("Client is not known")
+            conn.close()
+            continue
+        
+        print("Total key is generated")
+        K = total_key_gen(Kpc)
+        conn.send(cript("Welcome!", K).encode())        
+        
+        while True:
+            try:
+                data = conn.recv(1024).decode()
+            except:
+                break
+            data = decript(data, K)
+            print(data)
+            msg = "Your message recieved: " + data
+            msg = cript(msg, K)
+            conn.send(msg.encode())
+        
+        conn.close()
+    
+    
+main()
