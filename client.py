a0dcc39e4e21611655e63e17085a7989ef991190
nect88 <55210686+nect88@users.noreply.github.com>
2019-12-24 18:43:55 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,14 +1,73 @@
 import socket
-import pickle
 
-HOST = '127.0.0.1'
-PORT = 8080
+def pc_key_gen(g, p, Ksc):
+     Kpc = str(g**Ksc % p)
+     return Kpc
+ 
+def total_key_gen(Kps, Ksc, p):
+    K = (Kps**Ksc) % p
+    return K
 
-sock = socket.socket()
-sock.connect((HOST, PORT))
+def cript(msg, k):
+    msgl = list(msg)
+    for i in range(len(msgl)):
+        msgl[i] = chr(ord(msgl[i])+k)
+    msg = ''.join(msgl)
+    return msg
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
+def decript(msg, k):
+    msgl = list(msg)
+    for i in range(len(msgl)):
+        msgl[i] = chr(ord(msgl[i])-k)
+    msg = ''.join(msgl)
+    return msg
 
-sock.close()
+def main():
+    
+    sock = socket.socket()
+    sock.setblocking(1)
+    sock.connect(('localhost', 9092))
+    
+    #Ksc = 101
+    Ksc = int(input('''Enter your secret key (for example 13):
+>'''))
+    
+    data = sock.recv(1024).decode()
+    data = data.split(' ')
+    g = int(data[1])
+    p = int(data[2])
+    Kps = int(data[0])
+    
+    Kpc = pc_key_gen(g, p, Ksc)
+    print("Your private key is generated correctly")
+    sock.send(Kpc.encode())
+    
+    K = total_key_gen(Kps, Ksc, p)
+    print("Total key is generated\n")
+    
+    data = sock.recv(1024).decode()
+    data = decript(data, K)
+    print(data)
+    
+    while True:
+        msg = input()
+        if msg == 'ex':
+            break
+        else:
+            msg = cript(msg, K)
+        
+            sock.send(msg.encode())
+
+        try:
+            data = sock.recv(1024).decode()
+        except:
+            print("Server stopped the conversation")
+            break
+        data = decript(data, K)
+        print(data)
+        
+        
+    sock.close()
+
+
+main()
